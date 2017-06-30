554adc800bf2126be56117a1c476ccbb08cbe24f
AROPTIONS\sdalton <glypher@gmail.com>
2017-06-30 17:08:37 -0500

    more local registration stuff..

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
 Route::group(['middleware' => ['cors']], function () {
 
     //register
+    Route::post('/Register', 'LocalAuthController@register');
     //login
     //verify
     //resendemail
